ae905470c4c5f0e401e50950a267481973f80f37
antonov548 <antonov5551998@gmail.com>
2021-05-29 18:23:49 +0300

    cmake: snark. Gmp changed from 64 to 32. static_assert failed for emcc

--- libs/include/gmp.h
+++ libs/include/gmp.h
@@ -40,7 +40,7 @@ see https://www.gnu.org/licenses/.  */
 #if ! defined (__GMP_WITHIN_CONFIGURE)
 #define __GMP_HAVE_HOST_CPU_FAMILY_power   0
 #define __GMP_HAVE_HOST_CPU_FAMILY_powerpc 0
-#define GMP_LIMB_BITS                      64
+#define GMP_LIMB_BITS                      32
 #define GMP_NAIL_BITS                      0
 #endif
 #define GMP_NUMB_BITS     (GMP_LIMB_BITS - GMP_NAIL_BITS)
@@ -2323,8 +2323,8 @@ enum
 };
 
 /* Define CC and CFLAGS which were used to build this version of GMP */
-#define __GMP_CC "gcc -m64"
-#define __GMP_CFLAGS "-pipe -O1    "
+#define __GMP_CC "/home/antonov548/emsdk/upstream/emscripten/emcc"
+#define __GMP_CFLAGS "-O2 -pedantic"
 
 /* Major version number is the value of __GNU_MP__ too, above. */
 #define __GNU_MP_VERSION            6
